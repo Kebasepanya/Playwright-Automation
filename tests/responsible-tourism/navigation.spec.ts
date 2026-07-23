3df2b7eaf2c759087c509e604de1ff98d8017a1f
Kebasepanya <Kebaabetswe1.sepanya@gmail.com>
2026-07-23 19:40:04 +0200

    Responsibility tourism sections

--- tests/responsible-tourism/navigation.spec.ts
+++ tests/responsible-tourism/navigation.spec.ts
@@ -7,3 +7,22 @@ test('Responsible Tourism', async ({ page }) => {
     await expect(page.getByText('Responsible Tourism')).toBeVisible();
 });
 
+test('The Good We Do Section', async ({ page }) => {
+    await page.goto('https://hub.dev.wetu.com/profile/348615/responsible-tourism');
+    await expect(page.getByRole('heading', { name: 'The Good We Do' })).toBeVisible();
+});
+
+test('Certifications section', async ({ page }) => {
+    await page.goto('https://hub.dev.wetu.com/profile/348615/responsible-tourism');
+    await expect(page.getByRole('heading', { name: 'Certifications' })).toBeVisible();
+});
+
+test('Sustainability Profile Section', async ({ page }) => {
+    await page.goto('https://hub.dev.wetu.com/profile/348615/responsible-tourism');
+    await expect(page.getByRole('heading', { name: 'Sustainability Profile' })).toBeVisible();
+});
+
+test('SGovernance Section', async ({ page }) => {
+    await page.goto('https://hub.dev.wetu.com/profile/348615/responsible-tourism');
+    await expect(page.getByRole('heading', { name: 'Governance' })).toBeVisible();
+});
